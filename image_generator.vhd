8882bc7710f21dba6d0838f078d8064ed9203470
Prady Niraula <Prkn.2000@gmail.com>
2023-01-28 17:30:16 -0500

    vga ctrl done, img gen just needs vivado roms

--- image_generator.vhd
+++ image_generator.vhd
@@ -14,32 +14,52 @@ entity image_generator is
 		vid_en    : in std_logic;
 		
 		ball_x    : in std_logic_vector(WIDTH-1 downto 0);
-        ball_y    : in std_logic_vector(WIDTH-1 downto 0);
-        left_pad  : in std_logic_vector(WIDTH-1 downto 0);
-        right_pad : in std_logic_vector(WIDTH-1 downto 0);
+		ball_y    : in std_logic_vector(WIDTH-1 downto 0);
+		left_pad  : in std_logic_vector(WIDTH-1 downto 0);
+		right_pad : in std_logic_vector(WIDTH-1 downto 0);
 		score1    : in integer;
 		score2	  : in integer;
 		
-        red, green, blue : out std_logic_vector(3 downto 0));
+		red, green, blue : out std_logic_vector(3 downto 0));
 end image_generator;
 
 architecture BHV of image_generator is
 	
-	signal ROM_ADDRESS : std_logic_vector(9 downto 0);
+	--rom addresses
+	signal score_rom_addr : std_logic_vector(9 downto 0);
+	signal ball_rom_addr  : std_logic_vector(7 downto 0);
+	
+	--enable signals
 	signal PS_en : std_logic_vector(1 downto 0);
 	signal Score_en : std_logic; 
+	signal ball_en : std_logic;
+	
+	--rgb values
+	type   scores_t is array(0 to 9) of std_logic_vector(11 downto 0); 
+	signal rgb_scores : scores_t;
+	signal rgb_ball : std_logic_vector(11 downto 0);
 
 begin
 		
-	U_BLOCK_COL : entity work.block_col port map (
+	-- score display
+	U_BLOCK_COLS : entity work.block_col port map (
 		Hcount => h_count,
-		Col_Addr => ROM_ADDRESS(4 downto 0),
+		Col_Addr => score_rom_addr(4 downto 0),
 		PS_en => PS_en);
 		
-	U_BLOCK_ROW : entity work.block_row port map (
+	U_BLOCK_ROWS : entity work.block_row port map (
 		Vcount => v_count,
-		Row_Addr => ROM_ADDRESS(9 downto 5),
+		Row_Addr => score_rom_addr(9 downto 5),
 		Score_en => Score_en);	
+		
+	--ball display
+	U_BLOCK_BALL : entity work.block_row_col_ball port map (
+		Vcount => v_count, 
+		Hcount => h_count, 
+		ball_x => ball_x,
+		ball_y => ball_y,
+		Rom_Addr => ball_rom_addr,
+		Ball_en => ball_en);	
 		
 		
 	-- need to include 11 vivado rom instances (10 digits 32x32, 1 ball 16x16)
@@ -54,14 +74,23 @@ begin
 			
 			--score
 			if (Score_en = '1' and PS_en = "01") then
-				--display p1 score from rom
+				--display p2 score from rom
+				red   <= rgb_scores(score1)(11 downto 8);
+				green <= rgb_scores(score1)(7  downto 4);
+				blue  <= rgb_scores(score1)(3  downto 0);
 			elsif (Score_en = '1' and PS_en = "10") then
 				--display p2 score from rom
+				red   <= rgb_scores(score2)(11 downto 8);
+				green <= rgb_scores(score2)(7  downto 4);
+				blue  <= rgb_scores(score2)(3  downto 0);
 			end if;
 			
 			--ball
-			if (unsigned(h_count) >= unsigned(ball_x) and unsigned(h_count) <= unsigned(ball_x)+15 and unsigned(v_count) >= unsigned(ball_y) and unsigned(v_count) <= unsigned(ball_y)+15 ) then
+			if (ball_en = '1') then
 				--display ball from rom
+				red   <= rgb_ball(11 downto 8);
+				green <= rgb_ball(7  downto 4);
+				blue  <= rgb_ball(3  downto 0);
 			end if;
 			
 			--paddle1
