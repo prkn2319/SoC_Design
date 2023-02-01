c06a54bac2f624f8815ad95e81c4759b209017c0
cvbosch <58001252+cvbosch@users.noreply.github.com>
2023-02-01 14:35:41 -0500

    gamecontroller update

--- game_controller.vhd
+++ game_controller.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 --x 640p
 --y 480p
@@ -36,7 +37,7 @@ architecture arch of game_controller is
 
 begin
 
-    process (rst, clock)
+    process (rst, clk)
     begin 
 
         if (rst = '1' OR game_reset <= '1') then
@@ -56,16 +57,16 @@ begin
             
             -- PADDLE LOGIC
             if (in1 = '1' AND to_integer(left_pad_pos) >= 10) then
-                left_pad_pos = left_pad_pos - 10;
+                left_pad_pos <= left_pad_pos - 10;
             end if;
             if (in2 = '1' AND to_integer(left_pad_pos) <= 420) then
-                left_pad_pos = left_pad_pos + 10;
+                left_pad_pos <= left_pad_pos + 10;
             end if;
             if (in3 = '1' AND to_integer(right_pad_pos) >= 10) then
-                right_pad_pos = right_pad_pos - 10;
+                right_pad_pos <= right_pad_pos - 10;
             end if;
             if (in4 = '1' AND to_integer(right_pad_pos) <= 420) then
-                right_pad_pos = right_pad_pos + 10;
+                right_pad_pos <= right_pad_pos + 10;
             end if;
             
             -- GAME LOGIC
@@ -73,32 +74,32 @@ begin
                 --ball contacts
                 direction_right <= '1';
                 if (ball_ypos >= left_pad_pos AND ball_ypos < left_pad_pos + 10) then
-                    ball_yvect <= -10;
+                    ball_yvect <= to_signed(-10, WIDTH);
                 elsif (ball_ypos >= left_pad_pos + 10 AND ball_ypos < left_pad_pos + 20) then
-                    ball_yvect <= -5;
+                    ball_yvect <= to_signed(-5, WIDTH);
                 elsif (ball_ypos >= left_pad_pos + 20 AND ball_ypos < left_pad_pos + 30) then
-                    ball_yvect <= 0;
+                    ball_yvect <= to_signed(0, WIDTH);
                 elsif (ball_ypos >= left_pad_pos + 30 AND ball_ypos < left_pad_pos + 40) then
-                    ball_yvect <= 5;
+                    ball_yvect <= to_signed(5, WIDTH);
                 elsif (ball_ypos >= left_pad_pos + 40 AND ball_ypos < left_pad_pos + 50) then
-                    ball_yvect <= 10;
+                    ball_yvect <= to_signed(10, WIDTH);
                 --ball misses
                 else
-                    direction_right <= '0'
+                    direction_right <= '0';
                 end if;
             elsif (direction_right = '1' AND to_integer(ball_xpos) = 620) then
                 --ball contacts
-                direction_right <= '0'
+                direction_right <= '0';
                 if (ball_ypos >= right_pad_pos AND ball_ypos < right_pad_pos + 10) then
-                    ball_yvect <= -10;
+                    ball_yvect <= to_signed(-10, WIDTH);
                 elsif (ball_ypos >= right_pad_pos + 10 AND ball_ypos < right_pad_pos + 20) then
-                    ball_yvect <= -5;
+                    ball_yvect <= to_signed(-5, WIDTH);
                 elsif (ball_ypos >= right_pad_pos + 20 AND ball_ypos < right_pad_pos + 30) then
-                    ball_yvect <= 0;
+                    ball_yvect <= to_signed(0, WIDTH);
                 elsif (ball_ypos >= right_pad_pos + 30 AND ball_ypos < right_pad_pos + 40) then
-                    ball_yvect <= 5;
+                    ball_yvect <= to_signed(5, WIDTH);
                 elsif (ball_ypos >= right_pad_pos + 40 AND ball_ypos < right_pad_pos + 50) then
-                    ball_yvect <= 10;
+                    ball_yvect <= to_signed(10, WIDTH);
                 --ball misses
                 else
                     direction_right <= '1';
@@ -107,22 +108,22 @@ begin
             elsif (ball_ypos = 0 OR ball_ypos = 480) then
                 ball_yvect <= -ball_yvect;
             -- ball hits side, game resets
-            elseif (ball_xpos = 0 OR ball_xpos = 640) then
+            elsif (ball_xpos = 0 OR ball_xpos = 640) then
                 game_reset <= '1';
             end if;
 
             -- ASSIGN OUTPUT (may need to make outputs combinational to avoid clock cycle delay)
-            if (directon_right = '0') then
+            if (direction_right = '0') then
                 ball_xpos <= ball_xpos - 10;
-                ball_x <= ball_xpos - 10;
+                ball_x <= std_logic_vector(ball_xpos - 10);
             else
                 ball_xpos <= ball_xpos + 10;
-                ball_x <= ball_xpos + 10;
+                ball_x <= std_logic_vector(ball_xpos + 10);
             end if;
-            ball_ypos = ball_ypos + ball_yvect;
-            ball_y = ball_ypos + ball_yvect;
-            left_pad = left_pad_pos;
-            right_pad = right_pad_pos;
+            ball_ypos <= unsigned(signed(ball_ypos) + ball_yvect);
+            ball_y <= std_logic_vector(signed(ball_ypos) + ball_yvect);
+            left_pad <= std_logic_vector(left_pad_pos);
+            right_pad <= std_logic_vector(right_pad_pos);
 
 
         end if;
