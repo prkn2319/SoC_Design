79043e7dc38b6d1e9d8be4850e2efe4d4fc7631d
cvbosch <58001252+cvbosch@users.noreply.github.com>
2023-02-02 13:20:58 -0500

    TB skeleton

---
+++ game_controller_tb.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity game_controller_tb is
+end game_controller_tb;
+
+
+architecture testbench of game_controller_tb is
+
+
+
+begin
+
+
+
+end testbench;
